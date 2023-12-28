9a42fc2d2a96df0ff980973ee4c44ba5491faade
SeminKang <ksm9802@gmail.com>
2023-12-28 11:37:00 +0900

    [NextJS-zcom] 섹션1_5강

--- NextJS/z-com/src/app/page.tsx
+++ NextJS/z-com/src/app/page.tsx
@@ -5,15 +5,17 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <div>
+    <>
       <div className={styles.left}>
         <Image src={zlogo} alt="logo" />
       </div>
       <div className={styles.right}>
         <h1>지금 일어나고 있는 일</h1>
         <h2>지금 가입하세요.</h2>
-        <Link href="/login" className="">로그인</Link>
+        <Link href="/i/flow/signup" className={styles.signup}>계정 만들기</Link>
+        <h2>이미 트위터에 가입하셨나요?</h2>
+        <Link href="/login" className={styles.login}>로그인</Link>
       </div>
-    </div> 
+    </> 
   )
 }
